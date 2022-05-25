48df260760e89ac2da72dae5dba48720adf2fe99
grand-magus <grandmagus703@gmail.com>
2022-05-25 06:02:08 -0400

    ci(config): added eslint, typescript and babel configs

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+    root: true,
+    extends: '@react-native-community',
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'import'],
+    overrides: [
+      {
+        files: ['*.ts', '*.tsx'],
+        rules: {
+          '@typescript-eslint/no-shadow': ['error'],
+          'no-shadow': 'off',
+          'no-undef': 'off',
+          'import/order': [
+            1,
+            {
+              'newlines-between': 'always',
+              groups: [
+                'external',
+                'builtin',
+                'index',
+                'internal',
+                'sibling',
+                'parent',
+                'object',
+                'type',
+              ],
+              pathGroups: [
+                {
+                  pattern: 'react**',
+                  group: 'external',
+                },
+                {
+                  pattern: '~services/**',
+                  group: 'index',
+                },
+                {
+                  pattern: '~components/**',
+                  group: 'internal',
+                },
+                {
+                  pattern: '~screens/**',
+                  group: 'internal',
+                },
+                {
+                  pattern: './**',
+                  group: 'internal',
+                  position: 'after',
+                },
+                {
+                  pattern: '~hooks/**',
+                  group: 'internal',
+                  position: 'after',
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ],
+  };
+  
--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { useColorScheme } from 'react-native';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from '~screens/home'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const scheme = useColorScheme();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
