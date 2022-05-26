674ef940328baf40b15d904336722c7fd082b74b
grand-magus <grandmagus703@gmail.com>
2022-05-26 06:36:29 -0400

    feat(layout): updated root layout

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
 import * as React from 'react';
 import { useColorScheme } from 'react-native';
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 
-import Home from '~screens/home'
+import Breeds from '~screens/breeds'
+import Details from '~screens/details'
 
 const Stack = createNativeStackNavigator();
+const queryClient = new QueryClient()
 
 export default function App() {
   const scheme = useColorScheme();
+
   return (
-    <NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack.Navigator>
+          <Stack.Screen name="Breeds" component={Breeds} />
+          <Stack.Screen name="Details" component={Details} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
